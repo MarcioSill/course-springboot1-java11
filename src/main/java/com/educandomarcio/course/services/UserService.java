477cf95e53b407c71ef511c92a18284277b8a0d2
MarcioSill <marcio.sill@hotmail.com>
2022-02-16 17:50:50 -0300

    Service layer, component registration git commit -m

---
+++ src/main/java/com/educandomarcio/course/services/UserService.java
@@ -0,0 +1,31 @@
+package com.educandomarcio.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandomarcio.course.entities.User;
+import com.educandomarcio.course.repositories.UserRepository;
+
+//registra como componente do Spring com a anotação
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	//retorna todo usuarios do banco de dados
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	//recuperando por ID
+	public User finById(long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
