45722dd24c2ad5826b949b7ad3717cfcc4bd1d3e
MarcioSill <marcio.sill@hotmail.com>
2022-04-29 22:03:29 -0300

    Exception handling - delete

--- src/main/java/com/educandomarcio/course/services/UserService.java
+++ src/main/java/com/educandomarcio/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandomarcio.course.entities.User;
 import com.educandomarcio.course.repositories.UserRepository;
+import com.educandomarcio.course.services.exceptions.DatabaseException;
 import com.educandomarcio.course.services.exceptions.ResourceNotFoundException;
 
 
@@ -35,7 +38,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	
